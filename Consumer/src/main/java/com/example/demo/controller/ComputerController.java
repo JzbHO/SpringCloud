35f8410130378be88a659b980b4bf1b31f571b5f
JzbHo <3286056707@qq.com>
2017-11-17 21:26:10 +0800

    服务消费实现

---
+++ Consumer/src/main/java/com/example/demo/controller/ComputerController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+;
+
+/**
+ * Created by Administrator on 2017/11/16 0016.
+ */
+
+@RestController
+public class ComputerController {
+
+    @Autowired
+    RestTemplate restTemplate;
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return restTemplate.getForEntity("http://compute-service/test", String.class).getBody();
+    }
+}
---
+++ product/src/main/java/com/example/demo/ComputerController.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import com.netflix.appinfo.InstanceInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/11/16 0016.
+ */
+
+@RestController
+public class ComputerController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComputerController.class);
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/test")
+    public String index(){
+        System.out.println("coming....");
+        ServiceInstance instance = serviceInstance();
+        LOGGER.info("provider service, host = " + instance.getHost()
+                + ", service_id = " + instance.getServiceId());
+        return "Hello,Provider!";
+    }
+
+
+
+    public ServiceInstance serviceInstance() {
+        List<ServiceInstance> list = discoveryClient.getInstances("compute-service");
+        if (list != null && list.size() > 0) {
+            return (ServiceInstance) list.get(0);
+        }
+        return null;
+    }
+}
